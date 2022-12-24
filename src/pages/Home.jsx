4b4886a8d6e168f526f50ea7626f6905aed02501
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-12-24 19:19:45 +0200

    first part

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,21 @@
 import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { getCountries } from 'service/country-service';
+import { useState, useEffect } from 'react';
 
 export const Home = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const countriesArr = await getCountries();
+      setCountries(countriesArr);
+    };
+    fetchCountries();
+  }, []);
   return (
     <Section>
       <Container>
-        <h2>Home</h2>
+        <CountryList countries={countries} />
       </Container>
     </Section>
   );
